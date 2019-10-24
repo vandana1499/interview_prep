fd3c2b808dab326e4c7a4cc15240d997391a235f
unbeat <vandanagupta1499@gmail.com>
2019-10-24 14:10:30 +0530

    Add files via upload

---
+++ bit_manipulation/Binary_AND_numbers_in_range.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int c=0;
+       
+       while(m!=n)
+       {
+           m=m>>1;
+           n=n>>1;
+           c++;
+       }
+      
+        return m<<c;
+    }
+}
