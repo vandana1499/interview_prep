25dbdad7c6f309dd9c42f35be176bf79de0fbd42
unbeat <vandanagupta1499@gmail.com>
2019-10-25 13:44:32 +0530

    Add files via upload

---
+++ bit_manipulation/SumvsXOR_hackerrank.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the sumXor function below.
+    static long sumXor(long n) {
+
+       if(n==0)
+        return 1;
+        if((n&n-1)==0)
+          return n;
+       
+          long count=0;
+          while(n>0)
+          {
+              if((n&1)==0)
+                count+=1;
+                n=n>>1;
+               
+          }
+          return 1<<count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = sumXor(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
